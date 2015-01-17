bc6ec18a194066d55fe29a6f8fcd17d209fbf82f
Vclcl <chamesmuhametov@yandex.ru>
2015-01-17 15:34:15 +0400

    CalcView data annotation

--- MvcApplication/Models/CalcView.cs
+++ MvcApplication/Models/CalcView.cs
@@ -9,11 +9,26 @@ namespace MvcApplication.Models
 {
     public class CalcView
     {
+        [Display(Name = "Сумма в валюте")]
+        [Required(ErrorMessage = "Сумма в валюте незаполнена")]
         public decimal Sum { get; set; }
+
+        [Display(Name = "Курс")]
+        [Required(ErrorMessage = "Курс валюты незаполнен")]
         public decimal ExchangeRate { get; set; }
+
+        [Display(Name = "НДС")]
+        [Required(ErrorMessage = "НДС незаполнен")]
         public decimal NDS { get; set; }
+
+        [Display(Name = "Количество платежей")]
+        [Required(ErrorMessage = "Количество платежей незаполнено")]
         public int CountPayment { get; set; }
+
+        [Display(Name = "Дата первого платежа")]
+        [Required(ErrorMessage = "Дата первого платежа незаполнена")]
         public DateTime FirstDate { get; set; }
+
         IEnumerable<Payment> PaymentShedule { get; set;}
     }
 }
