be343f47a5db09f9765ffba601d5da448deeaf9a
Vclcl <chamesmuhametov@yandex.ru>
2015-01-17 12:34:44 +0400

    CalcPay and Payment type add

---
+++ CalcModel/CalcPay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcModel
+{
+    public class CalcPay
+    {
+        public decimal Sum { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public decimal NDS { get; set; }
+        public int CountPayment { get; set; }
+        public DateTime FirstDate { get; set; }
+    }
+}
---
+++ CalcModel/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcModel
+{
+    public class Payment
+    {
+        public DateTime Date { get; set; }
+        public decimal Sum { get; set; }
+    }
+}
