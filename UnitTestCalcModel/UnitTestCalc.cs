1d125c10fef8d87e0b08e685ac46bf14119c3976
Vclcl <chamesmuhametov@yandex.ru>
2015-01-17 13:09:34 +0400

    Total sum test implement and passed

---
+++ UnitTestCalcModel/UnitTestCalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcModel;
+
+namespace UnitTestCalcModel
+{
+    [TestClass]
+    public class UnitTestCalc
+    {
+        [TestMethod]
+        public void TestTotalSum()
+        {
+            CalcPay calc = new CalcPay();
+            calc.Sum = 125000m;
+            calc.ExchangeRate = 33.78m;
+            calc.NDS = 18m;
+            Assert.AreEqual(calc.GetTotalSum(), 4982550m);
+        }
+    }
+}
