93a5f3be991149088c2a1f7c83752ed268975693
Vclcl <chamesmuhametov@yandex.ru>
2015-01-17 15:23:36 +0400

    MVC Project start

---
+++ MvcApplication/Models/CalcView.cs
@@ -0,0 +1,19 @@
+﻿using CalcModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication.Models
+{
+    public class CalcView
+    {
+        public decimal Sum { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public decimal NDS { get; set; }
+        public int CountPayment { get; set; }
+        public DateTime FirstDate { get; set; }
+        IEnumerable<Payment> PaymentShedule { get; set;}
+    }
+}
