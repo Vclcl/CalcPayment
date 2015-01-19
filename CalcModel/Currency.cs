4f6dd29acd390cfd8cc6e8285604be7e1ae77439
Vclcl <chamesmuhametov@yandex.ru>
2015-01-19 16:04:31 +0400

    Currency exchange rate repository model

---
+++ CalcModel/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcModel
+{
+    public class CurrencyState
+    {
+        public string Name { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public CurrencyState(string name, decimal exchangeRate)
+        {
+            Name = name;
+            ExchangeRate = exchangeRate;
+        }
+    }
+}
---
+++ CalcModel/RateRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcModel
+{
+    public class RateRepository
+    {
+        CurrencyState[] _currencyList = { new CurrencyState("USD", 64.422m),
+                                          new CurrencyState("EUR", 74.693m),
+                                          new CurrencyState("GBP", 98.3889m)};
+        IEnumerable<string> CurrencyCollection()
+        {
+            return _currencyList.Select(x => x.Name);
+        }
+        decimal ExchangeRate(string currencyName)
+        {
+            CurrencyState instance = _currencyList.FirstOrDefault(x => x.Name == currencyName);
+            if (instance != null)
+            {
+                return instance.ExchangeRate;
+            }
+            else
+            {
+                return 0m;
+            }
+        }
+    }
+}
